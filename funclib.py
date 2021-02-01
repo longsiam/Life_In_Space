4ae24d7e86701b867ef7970d18e77ad6921bd2e6
longsiam <78302446+longsiam@users.noreply.github.com>
2021-02-01 16:42:08 +0800

    Update funclib.py

--- funclib.py
+++ funclib.py
@@ -1,7 +1,49 @@
 # function library of LifeInSpace
 # version: BETA_00
-import pygame
+import os
 import sys
+import pygame
 
-def quit_game():
-	sys.exit(0)
+def quit_game(datpck: dict):
+	'''
+	安全退出游戏
+	datpck: 游戏数据包
+	'''
+	save_file(datpck)  # 先存档
+	sys.exit(0)        # 退出码为0：安全退出
+
+def encry(target: str) -> str:
+	'''
+	加密一段字符串
+	target: 要加密的字符串
+	返回值: 加密后的字符串
+	'''
+	return "".join(reversed([chr((ord(x)^3)+5) for x in target]))
+
+def decry(target: str) -> str:
+	'''
+	解密一段字符串
+	target: 被加密的字符串
+	返回值: 解密后的字符串
+	'''
+	return "".join(reversed([chr((ord(x)-5)^3) for x in target]))
+
+def save_file(datpck: dict):
+	'''
+	将数据存档
+	datpck: 游戏数据包
+	'''
+	# os.getcwd()能获取当前路径
+	# .dat文件其实就是普通的文本文件换了个后缀, player.dat是游戏存档文件
+	with open(os.getcwd()+r"\player.dat", "w") as f:
+		# 将统计信息与进度描述放入一个元组，写入存档中
+		f.write(str((datpck["stats"], datpck["progress"])))
+
+def read_file() -> tuple:
+	'''
+	读取存档
+	返回值：统计信息与进度的元组
+	'''
+	with open(os.getcwd()+r"\player.dat", "r") as f:
+		# 此时读得的是字符串，我们要eval一下才是真正的元组
+		return eval(f.read())
