5aec2c68a340f41cebf2c08b187c2d1e3f3df6a8
longsiam <78302446+longsiam@users.noreply.github.com>
2021-02-01 17:56:40 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -2,4 +2,10 @@
 # version: BETA_00
 class setting:
 	'''储存游戏设置'''
-	pass
+	def __init__(self):
+		self.screen_width=1050 # 游戏窗口宽度
+		self.screen_high=660   # 游戏窗口高度
+		self.init_dynamic()    # 初始化动态设置
+	def init_dynamic(self):
+		'''初始化动态设置'''
+		pass
