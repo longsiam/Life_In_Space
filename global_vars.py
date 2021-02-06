76e77ee619f67154fcdb12ea7485f5544de05d47
longsiam <78302446+longsiam@users.noreply.github.com>
2021-02-06 15:46:35 +0800

    Update global_vars.py

--- global_vars.py
+++ global_vars.py
@@ -1,5 +1,18 @@
-# 可能被其他文件修改的全局变量
+# 可能被其他文件修改的全局变量与常用库
 # version: BETA
+
+# 导入常用的库
+try:
+	import pygame
+except ImportError:
+	# 这个是第三方库，如果没安装就开pip安装
+	from pip._internal import main
+	main.main(["install","pygame"])
+from os import system, getcwd
+from threading import Thread
+from time import sleep
+from random import randint,choice
+
 
 datpck = {}
 def change_datpck(key,value):
