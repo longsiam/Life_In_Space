895bfe891fd8d95c1b2904e90f40775075ec47bb
longsiam <78302446+longsiam@users.noreply.github.com>
2021-02-06 16:10:45 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -9,13 +9,13 @@ class Setting:
 		self.screen_width = 1050  # 游戏窗口宽度
 		self.screen_high = 660    # 游戏窗口高度
 		self.start_money = 2000.0 # 玩家起始财产
-		self.star_density = 30    # 后生成星星密度
+		self.star_density = 30    # 移动生成星星密度
 		self.star_edge = 15       # 星星距窗口边框的距离(单位px)
-		self.star_quantity = 75   # 一次性生成的星星数量
-		self.star_darkest = 45    # 最暗的星星亮度
+		self.star_quantity = 55   # 一次性生成的星星数量
+		self.star_darkest = 40    # 最暗的星星亮度
 		self.star_lightest = 230  # 最亮的星星亮度 
 		self.star_size = 2        # 星星尺寸(单位px) 
-		self.fade_time = 1        # 淡入/淡出所需时间
+		self.fade_time = 0.2      # 淡入/淡出所需时间
 		# 星星颜色RGB值表
 		self.star_color = lambda light: (
 			(  light,    light,   light  ),  # 白色
@@ -28,6 +28,8 @@ class Setting:
 			( light/2,  light/2,  light  ),  # 蓝色
 			(  light,    light,  light/2 )   # 黄色
 		)
+		# 起始CG换画面停顿时间
+		self.scg_chgimg_time = (2,2.5,2.5,2.5,2.5,2.5,2.5,20,1.5,2.5)
 		self.init_dynamic()       # 初始化动态设置
 
 	def init_dynamic(self):
@@ -36,7 +38,7 @@ class Setting:
 			self.player_maxhp = 100  # 玩家血量最大值 
 			self.volume = 0.8        # 音响音量
 		else:
-			data=read_file()["setting"]
+			data = read_file()["setting"]
 			self.player_maxhp = data["pmaxhp"]
 			self.volume = data["volume"]
 			
@@ -47,6 +49,6 @@ class Setting:
 		'''
 		return {"pmaxhp":self.player_maxhp, "volume":self.volume}
 
-	def __str__(self):
+	def __repr__(self):
 		'''如果被当成字符串，那就返回动态设置的字符串（报错时要用）'''
-		return str(get_dynamic())
+		return repr(self.get_dynamic())
